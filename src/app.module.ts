3f0fd836b8d17bfdf277fb882be253955c267f28
hung.le <hung.le>
2022-11-19 22:37:25 +0700

    refactor: add global filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClinicModule } from './modules/clinic/clinic.module';
 import { SharedModule } from './shared/shared.module';
+import { HttpExceptionFilter } from './utils/filters/http-exception.filter';
 import { LoggerMiddleware } from './utils/middlewares/logger.middleware';
 
 @Module({
   imports: [ClinicModule, SharedModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
