558038612cd406e19fca60031647052e6df7471c
hung.le <hung.le>
2022-11-19 22:22:10 +0700

    feat: setup test instance

---
+++ src/utils/setup-ci-integration.ts
@@ -0,0 +1,50 @@
+import { HttpService } from '@nestjs/axios';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { AppModule } from '../app.module';
+import { ErrorResponseTransformInterceptor } from './interceptors/error-response-transform.interceptor';
+import { SuccessResponseTransformInterceptor } from './interceptors/success-response-transform.interceptor';
+import { useContainer } from 'class-validator';
+import { EnvConfig } from '../shared/configs/env.config';
+import { ScratchpayApiService } from '../shared/services/apis/scratchpay-api.service';
+export type SetupContinuousIntegrationTest = {
+  envConfig: EnvConfig;
+  httpService: HttpService;
+  app: INestApplication;
+  moduleFixture: TestingModule;
+  scratchpayApiService: ScratchpayApiService;
+};
+
+export async function setupContinuousIntegrationTest(): Promise<SetupContinuousIntegrationTest> {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+  const httpService = moduleFixture.get<HttpService>(HttpService);
+  const envConfig = moduleFixture.get<EnvConfig>(EnvConfig);
+
+  const scratchpayApiService =
+    moduleFixture.get<ScratchpayApiService>(ScratchpayApiService);
+
+  const app = moduleFixture.createNestApplication();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.use(new LoggerMiddleware().use);
+  app.useGlobalInterceptors(
+    new SuccessResponseTransformInterceptor(),
+    new ErrorResponseTransformInterceptor(),
+  );
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  await app.init();
+
+  return {
+    httpService,
+    envConfig,
+    app,
+    moduleFixture,
+    scratchpayApiService,
+  };
+}
