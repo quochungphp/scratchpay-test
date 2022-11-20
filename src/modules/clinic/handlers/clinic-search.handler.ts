44c4c2a95065a5c3d9ffb37c0a806b647274266d
hung.le <hung.le>
2022-11-20 21:22:37 +0700

    feat: cache data to handler huge traffic

--- src/modules/clinic/handlers/clinic-search.handler.ts
+++ src/modules/clinic/handlers/clinic-search.handler.ts
@@ -1,9 +1,15 @@
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { IDentalResponse } from '../../../shared/services/apis/types/scratchpay.interface';
+import {
+  REDIS_CACHE_1_HOURS,
+  REDIS_CACHE_5_MINS,
+} from '../../../shared/services/redis/types/redis.type';
+import { redisCacheKey } from '../../../utils/generate-key';
 import { RequestContext } from '../../../utils/request-context';
 import { ClinicRepository } from '../repositories/clinic.repository';
 import { ClinicSearchQuery } from '../types/clinic-search.query';
-
+import * as md5 from 'md5';
+import { RedisCacheService } from '../../../shared/services/redis/redis-cache.service';
 /*
   Note: Which has responsible hanover input to repository, after get data from repository 
   which will process to match expectation data response
@@ -11,6 +17,8 @@ import { ClinicSearchQuery } from '../types/clinic-search.query';
 @Injectable()
 export class ClinicSearchHandler {
   @Inject() private readonly clinicRepository: ClinicRepository;
+
+  @Inject() private readonly redisCacheService: RedisCacheService;
   private readonly logger = new Logger(ClinicSearchHandler.name);
 
   async execute(
@@ -18,8 +26,15 @@ export class ClinicSearchHandler {
     query: ClinicSearchQuery,
   ): Promise<IDentalResponse[]> {
     try {
-      const clinics = await this.clinicRepository.getClinics(context, query);
-      return <IDentalResponse[]>clinics;
+      const key = redisCacheKey(`search-clinic:${md5(JSON.stringify(query))}`);
+
+      const results = await this.redisCacheService.softGetOrSet(
+        key,
+        async () => this.clinicRepository.getClinics(context, query),
+        REDIS_CACHE_5_MINS,
+        REDIS_CACHE_5_MINS,
+      );
+      return <IDentalResponse[]>results;
     } catch (error) {
       const { correlationId } = context;
       this.logger.error({
